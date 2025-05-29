e4887278c4bf1a831191a0321a374522daa85d3d
feliciashang <feliciashang@gmail.com>
2025-05-29 11:48:55 -0400

    add electron

---
+++ public/electron.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  if (app.isPackaged) {
+    win.loadFile(path.join(__dirname, 'index.html'));
+  } else {
+    win.loadURL('http://localhost:3000'); // React dev server URL
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
