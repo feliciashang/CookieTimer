0726c82faf146d05a4b13625e1b8f35233a537a1
feliciashang <feliciashang@gmail.com>
2025-05-31 18:15:11 -0400

    formatting homepage

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,48 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import cookieImage from './assets/cookieImage.jpg';
+
 
 function Home () {
-  return (
-    <div>
-      <h1>Cookie Timer</h1>
-      Which step are you on?
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', marginTop: '20px' }}>
-        <Link to="/dough">Chill dough</Link>
-        <Link to="/preheat">Preheat</Link>
-        <Link to="/bake">Bake</Link>
-        <Link to="/cool">Cool</Link>
-      </div>
-    </div>
-  )
-}
+    const steps = [
+        { path: '/dough', label: 'Chill dough', image: cookieImage },
+        { path: '/preheat', label: 'Preheat', image: cookieImage },
+        { path: '/bake', label: 'Bake', image: cookieImage },
+        { path: '/cool', label: 'Cool', image: cookieImage },
+  ];
+    return (
+        <div style={{textAlign: 'center'}}>
+            <h1 style={{fontSize: '3rem'}}>
+                Cookie Timer
+            </h1>
+            <p style={{fontSize: '2rem'}}>
+                Which step are you on?
+            </p>
+            <div style={{ 
+                display: 'grid', 
+                gridTemplateColumns: 'repeat(2, 1fr)', 
+                gap: '1.5rem',
+                maxWidth: '800px',
+                margin: '0 auto',
+            }}>
+            {steps.map((({path, label, image}) => (
+                <Link 
+                key={path}
+                to={path}
+                >
+                    <img 
+                        src={image}
+                        alt={label}
+                        style={{ width: '45%', height: 'auto'}}
+                    />
+                    <div style={{fontSize: '1.5rem'}}>
+                        {label}
+                    </div>
+                </Link>
+            )))}
+        </div>
+        </div>
+    )
+    }
 
 export default Home
